2d77787bd0a9fa467fb5576e989bc75bc0b062d6
Proxlight <69844328+Proxlight@users.noreply.github.com>
2023-03-08 19:30:18 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,6 @@
+# Proxlight-Website
+
+## Light Mode
+<img align="center" src="/Screenshots/Light Mode.png" alt="Light Mode"></img>
+## Dark Mode
+<img align="center" src="/Screenshots/Dark Mode.png" alt="Dark Mode"></img>
---
+++ script.js
@@ -0,0 +1,11 @@
+document.addEventListener("mousemove",  parallax);
+function parallax(e){
+	this.querySelectorAll('.Shape').forEach(layer=> {
+		const speed = layer.getAttribute('data-speed')
+
+		const x = (window.innerWidth = e.pageX * speed) / 75
+		const y = (window.innerHeight = e.pageY * speed) / 75
+
+		layer.style.transform = `scale(0.5) translateX(${x}px) translateY(${y}px)`
+	})
+};
